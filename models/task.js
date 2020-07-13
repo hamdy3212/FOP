3c162d77e0eb21cfd0d16741ee8c34976f1ae903
hamdy <hamdyyousseif32@gmail.com>
2020-07-13 17:57:39 +0200

    create userSchema and start using authentication with passport

--- models/task.js
+++ models/task.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
-const taskSchema = new Schema({
+const Task = new Schema({
     author: String,
     title: String,
     selected: String,
     description: String,
 },
     { timestamps: true });
-const Task = mongoose.model('task', taskSchema);
-module.exports = Task;
+module.exports = mongoose.model('task', Task);
--- server.js
+++ server.js
@@ -1,34 +1,64 @@
-const   express  = require('express'),
-        app      = express(),
-        mongoose = require('mongoose'),
-        Task     = require('./models/task');
+const express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    Task = require('./models/task'),
+    User = require('./models/user'),
+    passport = require('passport'), //
+    LocalStrategy = require('passport-local'); //
+
+// connet to DB
+mongoose.connect('mongodb://localhost:27017/FOP', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+    .then(result => app.listen(3000))
+    .catch(err => console.log(err));
+
 // register view engine
 app.set('view engine', 'ejs');
 
+app.use(require('express-session')({
+    secret: "God help me please",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 // middleware and static folies
 app.use(express.static('public'));
-app.use(express.urlencoded( {extended: true}));
+app.use((req, res, next)=>{
+    res.locals.currentUser = req.user;
+    next();
+})
+app.use(express.urlencoded({ extended: true }));
+const isLoggedIn = (req, res, next)=>{
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
 
-// connect to DB
-mongoose.connect('mongodb://localhost:27017/FOP', {useNewUrlParser: true, useUnifiedTopology: true})
-.then( result => app.listen(3000) )
-.catch( err => console.log(err) )
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.render('index');
 })
-app.get("/tasks/tasklist", (req, res)=>{
+app.get("/tasks/tasklist",isLoggedIn, (req, res) => {
     Task.find()
-    .then( tasks => {
-        console.log(tasks);
-        res.render('tasks/tasklist', {tasks})}
+        .then(tasks => {
+            console.log(tasks);
+            res.render('tasks/tasklist', { tasks })
+        }
         )
-    .catch( err => console.log(err));
+        .catch(err => console.log(err));
 })
-app.get("/tasks/addtask", (req, res)=>{
+app.get("/tasks/addtask", (req, res) => {
     res.render('tasks/addtask');
 })
-app.post("/tasks/addtask", (req, res)=>{
+app.post("/tasks/addtask", (req, res) => {
     console.log(req.body)
     const task = new Task({
         author: "Nada",
@@ -37,10 +67,47 @@ app.post("/tasks/addtask", (req, res)=>{
         description: req.body.description
     });
     task.save()
-    .then((result)=>{
-        res.redirect('/tasks/tasklist');
+        .then((result) => {
+            res.redirect('/tasks/tasklist');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+// signup
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+app.post('/register', (req, res) => {
+    const newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password,
+    (err, user)=>{
+        if(err){
+            console.log(err)
+            return res.render('register');
+        }
+        passport.authenticate('local')(req, res, ()=>{
+            res.redirect("/");
+        });
+    });
+});
+
+// login
+app.get('/login', (req, res) => {
+    console.log(req.user);
+
+    res.render('login');
+})
+app.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login'
     })
-    .catch((err)=>{
-        console.log(err);
-    })
+);
+
+// logout
+app.get('/logout', (req, res)=>{
+    req.logOut();
+    res.redirect('/');
 })
