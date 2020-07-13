cdf35e9cdbb0a68a5c1e55cb942d25de5e090735
hamdy <hamdyyousseif32@gmail.com>
2020-07-13 07:39:29 +0200

    start using partials and add tasks routes

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const   express  = require('express'),
         app      = express();
 
+// register view engine
+app.set('view engine', 'ejs');
 
-app.set('view engine', 'ejs')
+// middleware and static folies
+app.use(express.static('public'));
+app.use(express.urlencoded( {extended: true}));
+const tasks = [];
+
+
 app.get("/", (req, res)=>{
     res.render('index');
 })
 app.get("/tasks/tasklist", (req, res)=>{
-    res.render('tasks/tasklist');
+    res.render('tasks/tasklist', {tasks});
 })
-
+app.get("/tasks/addtask", (req, res)=>{
+    res.render('tasks/addtask');
+})
+app.post("/tasks/addtask", (req, res)=>{
+    const taskname = req.body.taskname;
+    res.redirect('/tasks/addtask');
+    tasks.push(taskname);
+    console.log(tasks);
+})
 app.listen(3000)
